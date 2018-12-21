a54362ec7556880f5888accc19c16c42b4c6137a
Aleksandr <gra4ev.aleksander@yandex.ru>
2018-12-21 19:16:41 +0300

    Add bubbles

---
+++ Fasetto.Word/Controls/Chat/ChatMessage/ChatMessageListControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Fasetto.Word
+{
+    /// <summary>
+    /// Логика взаимодействия для ChatListControl.xaml
+    /// </summary>
+    public partial class ChatMessageListControl : UserControl
+    {
+        public ChatMessageListControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
