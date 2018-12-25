2154a909f528d873c561daf7c0e832d91a705ce0
Aleksandr <gra4ev.aleksander@yandex.ru>
2018-12-25 19:03:58 +0300

    Chat Message Bubble

---
+++ Fasetto.Word.Core/ViewModel/Chat/ChatMessage/Design/ChatMessageListDesignModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fasetto.Word.Core
+{
+    /// <summary>
+    /// The design-time data for a <see cref="ChatMessageListViewModel"/>
+    /// </summary>
+    public class ChatMessageListDesignModel : ChatMessageListViewModel
+    {
+        #region Singelton
+
+        /// <summary>
+        /// A single instance of the design model
+        /// </summary>
+        public static ChatMessageListDesignModel Instance => new ChatMessageListDesignModel();
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
+        public ChatMessageListDesignModel()
+        {
+            Items = new List<ChatMessageListItemViewModel>
+            {
+                new ChatMessageListItemViewModel
+                {
+                    Initials = "PL",
+                    SenderName = "Parnell",
+                    Message = "I'm about to wipe the old server. We need to update the old server to Windows 2016",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    SentByMe = false
+                },                       
+                new ChatMessageListItemViewModel
+                {
+                    Initials = "LM",
+                    SenderName = "Luke",
+                    Message = "Let me know when you manager to spin up the new 2016 server",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    MessageReadTime = DateTimeOffset.UtcNow.Subtract(TimeSpan.FromMinutes(5)),
+                    SentByMe = true,
+                },           
+                new ChatMessageListItemViewModel
+                {
+                    Initials = "PL",
+                    SenderName = "Parnell",
+                    Message = "The new server is up. Go to  192.168.1.1. \r\nUserName is admin, password is P8ssw0rd!",
+                    ProfilePictureRGB = "3099c5",
+                    MessageSentTime = DateTimeOffset.UtcNow,
+                    SentByMe = false
+                },
+            };
+        }
+
+        #endregion
+    }
+}
